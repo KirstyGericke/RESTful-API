3e97e7d52f3bf4900cd3459dd009ac65d2979c80
Kirsten Gericke <k.gericke@student.rug.nl>
2024-02-12 21:42:52 +0100

    controller logic step completed

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -4,6 +4,7 @@ import six
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
 
+from swagger_server.service.student_service import *
 
 def add_student(body=None):  # noqa: E501
     """Add a new student
@@ -15,9 +16,14 @@ def add_student(body=None):  # noqa: E501
 
     :rtype: float
     """
+    # if connexion.request.is_json:
+    #     body = Student.from_dict(connexion.request.get_json())  # noqa: E501
+    # return 'do some magic!'
+
     if connexion.request.is_json:
         body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        return add(body)
+    return 500, 'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +36,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +49,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
